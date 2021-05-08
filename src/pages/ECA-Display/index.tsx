ea6a8472e78418532bc12d0bebffd6b9c1a6e702
astatine-213-tian <318145208@qq.com>
2021-05-08 22:58:41 +0800

    missing files

--- src/pages/ECA-Display/index.tsx
+++ src/pages/ECA-Display/index.tsx
@@ -1,16 +1,21 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonGrid, IonRow, IonCol, IonPage, IonSearchbar, IonButton} from '@ionic/react';
+import React, { useState } from 'react';
 import './index.css';
 
 const ECADisplay: React.FC = () => {
+  const [searchText, setSearchText] = useState('');
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>展示</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-      </IonContent>
+      <IonRow className="ion-align-items-center">
+        <IonCol size="10">
+          <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="focus">
+          </IonSearchbar>
+        </IonCol>
+        <IonCol size="2">
+          <IonButton color="primary">筛选</IonButton>
+        </IonCol>
+      </IonRow>
+
     </IonPage>
   );
 };
