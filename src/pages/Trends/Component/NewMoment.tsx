021ce7ea3eea373f1078aa523208813d61a4523a
wuhaozhen <wuhaozhen1106@gmail.com>
2021-05-13 14:32:05 +0800

    Updated moments page

---
+++ src/pages/Trends/Component/NewMoment.tsx
@@ -0,0 +1,14 @@
+import {IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import React from "react";
+
+export default () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>发布动态</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  )
+}
