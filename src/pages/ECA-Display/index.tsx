0116939ead26c808bb3bcbd2a689f6f76875aef3
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-05-16 21:13:32 +0800

    Add ECA Display Card

--- src/pages/ECA-Display/index.tsx
+++ src/pages/ECA-Display/index.tsx
@@ -11,7 +11,7 @@ import {
   IonSlide
 } from '@ionic/react';
 import './index.css';
-import Card from "./Component/Card";
+import ECAInfoCard, {DisplayInfo} from "./Component/ECAInfoCard";
 
 const slideOpts = {
   initialSlide: 1,
@@ -25,45 +25,59 @@ const ecaData = {
   address: "深圳国际交流学院"
 }
 
-const ECADisplay: React.FC = () => {
-  const [searchText, setSearchText] = useState('');
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>
-            社团展示
-          </IonTitle>
-        </IonToolbar>
-        <IonToolbar>
-          <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="focus">
-          </IonSearchbar>
-          <IonButton slot="end" color="primary">筛选</IonButton>
-        </IonToolbar>
-      </IonHeader>
+class ECADisplay extends React.Component<any, any> {
+  async fetchData() {
+    return new Promise<void>( r => {
+      setTimeout(() => {
+        this.setState({
+          ecas: [{
+            poster: "http://placekitten.com/g/200/300",
+            name: "这是一个社团",
+            id: 400031,
+            size: 35,
+            address: "深圳国际交流学院",
+            posterProportion: 75,
+            cardHeight: 40,
+          }]
+        }, r)
+      }, 1000)
+    })
+  }
+  render() {
+    const [searchText, setSearchText] = useState('');
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>
+              社团展示
+            </IonTitle>
+          </IonToolbar>
+          <IonToolbar>
+            <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="focus">
+            </IonSearchbar>
+            <IonButton slot="end" color="primary">筛选</IonButton>
+          </IonToolbar>
+        </IonHeader>
 
-      <IonContent>
-        <IonSlides pager={true} options={slideOpts} scrollbar={true}>
-          <IonSlide>
-            <Card poster={ecaData.poster}
-                  name={ecaData.name}
-                  id={ecaData.id}
-                  size={ecaData.size}
-                  address={ecaData.address}
-                  posterProportion={75}
-                  height={40}/>
-          </IonSlide>
-          <IonSlide>
-            <h1>Slide 2</h1>
-          </IonSlide>
-          <IonSlide>
-            <h1>Slide 3</h1>
-          </IonSlide>
-        </IonSlides>
+        <IonContent>
+          <IonSlides pager={true} options={slideOpts} scrollbar={true}>
+            <IonSlide>
+              <ECAInfoCard info={this.state.ecas[0]}/>
+            </IonSlide>
+            <IonSlide>
+              <h1>Slide 2</h1>
+            </IonSlide>
+            <IonSlide>
+              <h1>Slide 3</h1>
+            </IonSlide>
+          </IonSlides>
 
-      </IonContent>
-    </IonPage>
-  );
-};
+        </IonContent>
+      </IonPage>
+    );
+  }
+
+}
 
 export default ECADisplay;
