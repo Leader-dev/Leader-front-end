f6f297490ecb3d991ef05471d7508cf119f4ee88
JokeNeverSoke <zengjoseph@hotmail.com>
2021-05-18 16:21:07 +0800

    Add /user/exist api wrapper

---
+++ src/services/user/exist.ts
@@ -0,0 +1,27 @@
+import axios from "@/utils/request";
+
+interface ExistsByPhoneProps {
+  phone: string;
+}
+
+interface ExistsByUsernameProps {
+  username: string;
+}
+
+export const userExist = async (
+  data: ExistsByPhoneProps | ExistsByUsernameProps
+): Promise<boolean> => {
+  if ("username" in data) {
+    return await axios
+      .post("/user/exist", { username: data.username })
+      .then((res) => {
+        return res.data.exist;
+      });
+  } else {
+    return await axios
+      .post("/user/exist", { phone: data.phone })
+      .then((res) => {
+        return res.data.exist;
+      });
+  }
+};
---
+++ src/services/user/index.ts
@@ -0,0 +1,1 @@
+export { userExist } from "./exist";
