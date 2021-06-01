a6f4de896216d3449e4db5619524ad43239ad18f
astatine-213-tian <318145208@qq.com>
2021-06-01 15:26:58 +0800

    Change pager position

--- src/pages/ECADisplay/Component/TopAdvertisement.tsx
+++ src/pages/ECADisplay/Component/TopAdvertisement.tsx
@@ -10,8 +10,20 @@ export interface AdvertisementInfo {
 export default ({ info }: { info: AdvertisementInfo }) => {
   const { id, posterUrl } = info;
   return (
-    <IonSlide>
-      <IonImg src={posterUrl} />
+    <IonSlide
+      style={{
+        borderRadius: "10px",
+        overflow: "hidden",
+      }}
+    >
+      <IonImg
+        src={posterUrl}
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        }}
+      />
       {/*TODO Onclick function*/}
     </IonSlide>
   );
