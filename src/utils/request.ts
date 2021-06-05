dcc7037776a68c652134bf5e911f5561954001c9
JokeNeverSoke <zengjoseph@hotmail.com>
2021-06-05 21:32:00 +0800

    Add axios interceptor for api token

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,38 @@
 import axios from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "/api",
   withCredentials: true,
 });
+
+// TODO: save using native APIs
+let currentKey: string;
+
+export const saveKey = (key: string) => {
+  currentKey = key;
+};
+
+export const getKey = (): string | null => {
+  return currentKey;
+};
+
+axiosInstance.interceptors.response.use((response) => {
+  if ("set-api-token" in response.headers) {
+    saveKey(response.headers["set-api-token"]);
+  }
+  return response;
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const k = getKey();
+  if (k) {
+    if (config.headers) {
+      config.headers["api-token"] = k;
+    } else {
+      config.headers = { "api-token": k };
+    }
+  }
+  return config;
+});
+
+export default axiosInstance;
