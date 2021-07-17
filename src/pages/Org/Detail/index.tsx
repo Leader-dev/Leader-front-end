c9eac98ee077ca56ce45b05714f4e5d174295fb9
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-07-18 00:14:02 +0800

    Use api to fetch data Bug fixed

--- src/pages/Org/Detail/index.tsx
+++ src/pages/Org/Detail/index.tsx
@@ -10,7 +10,6 @@ import {
   IonIcon,
   IonSegment,
   IonSegmentButton,
-  IonSkeletonText,
 } from "@ionic/react";
 import "./index.css";
 import { chevronBack, heartOutline, warningOutline } from "ionicons/icons";
@@ -22,59 +21,57 @@ import OrgDetailContact from "./Component/Contact";
 import InfoSkeleton from "./Component/InfoSkeleton";
 
 export default () => {
-  // TODO fetch data from backend
-  // const { orgId } = useParams<{ orgId: string }>();
-  // const { data: data, error } = useOrgDetails({ orgId });
-  // if (error) return <div>failed to load</div>
-  // if (!data) return (
-  //   <div> Skeleton </div>
-  // )
-
   // Test data
-  let data = {
-    detail: {
-      id: "xxxxxx",
-      name: "计算机协会",
-      numberId: 111111,
-      introduction:
-        "计算机协会成⽴于2004年10⽉10⽇，是学院最具潜⼒的学⽣社团之⼀，也是学院唯⼀⼀个科技类社团。协会是由⼴⼤的电脑爱好者⾃发组成，以“学习计算机知识，提⾼⾃⾝素质，相互帮助，团结协作”为宗旨，以“先进带动后进， 刻苦钻研计算机知识，勇攀IT科技⾼峰”为原则。积极⼤胆地⾛进计算机各个领域，不断总结交流，树⽴良好的计算机知识氛围，全⾯提⾼我院⼴⼤计算机爱好者们的计算机知识⽔平。",
-      memberCount: 280,
-      instituteName: "深国交",
-      address: "广东深圳",
-      addressAuth: "school",
-      typeAliases: [""],
-      posterUrl:
-        "https://tva1.sinaimg.cn/large/008i3skNgy1gsbsxdsc4ij30a006rjrp.jpg",
-      status: "",
-      phone: ["113115155", "189237523465"],
-      email: ["aaaaa@gmail.com", "test@qq.com"],
-      presidentName: "正在讲",
-      applicationScheme: {
-        open: false,
-        auth: true,
-        appointDepartment: true,
-        questions: [
-          { question: "question1", required: true },
-          { question: "question2", required: false },
-        ],
-      },
-    },
-    applicationStatus: "available",
-  };
+  // let data = {
+  //   detail: {
+  //     id: "xxxxxx",
+  //     name: "计算机协会",
+  //     numberId: 111111,
+  //     introduction:
+  //       "计算机协会成⽴于2004年10⽉10⽇，是学院最具潜⼒的学⽣社团之⼀，也是学院唯⼀⼀个科技类社团。协会是由⼴⼤的电脑爱好者⾃发组成，以“学习计算机知识，提⾼⾃⾝素质，相互帮助，团结协作”为宗旨，以“先进带动后进， 刻苦钻研计算机知识，勇攀IT科技⾼峰”为原则。积极⼤胆地⾛进计算机各个领域，不断总结交流，树⽴良好的计算机知识氛围，全⾯提⾼我院⼴⼤计算机爱好者们的计算机知识⽔平。",
+  //     memberCount: 280,
+  //     instituteName: "深国交",
+  //     address: "广东深圳",
+  //     addressAuth: "school",
+  //     typeAliases: [""],
+  //     posterUrl:
+  //       "https://tva1.sinaimg.cn/large/008i3skNgy1gsbsxdsc4ij30a006rjrp.jpg",
+  //     status: "",
+  //     phone: ["113115155", "189237523465"],
+  //     email: ["aaaaa@gmail.com", "test@qq.com"],
+  //     presidentName: "正在讲",
+  //     applicationScheme: {
+  //       open: false,
+  //       auth: true,
+  //       appointDepartment: true,
+  //       questions: [
+  //         { question: "question1", required: true },
+  //         { question: "question2", required: false },
+  //       ],
+  //     },
+  //   },
+  //   applicationStatus: "available",
+  // };
 
-  // Test skeleton
-  let loadingFirst = false;
-
+  // TODO fetch data from backend
+  const { orgId } = useParams<{ orgId: string }>();
+  const { data: orgDetail, error } = useOrgDetails({ orgId });
   const [tab, setTab] = useState<"info" | "contact">("info");
-  let orgContent, header;
-  if (loadingFirst) {
+
+  if (error) return <div>failed to load</div>;
+
+  let orgContent, backgroundUrl;
+  if (!orgDetail) {
     orgContent = <InfoSkeleton />;
+    backgroundUrl =
+      "https://tva1.sinaimg.cn/large/008i3skNgy1gsbsxdsc4ij30a006rjrp.jpg";
   } else {
     if (tab === "info") {
-      orgContent = <OrgDetailInfo info={data} />;
+      orgContent = <OrgDetailInfo info={orgDetail} />;
     } else {
-      orgContent = <OrgDetailContact info={data} />;
+      orgContent = <OrgDetailContact info={orgDetail} />;
     }
+    backgroundUrl = orgDetail.detail.posterUrl;
   }
 
   // TODO routing animation for back button
@@ -82,7 +79,7 @@ export default () => {
     <IonPage>
       <IonHeader
         style={{
-          backgroundImage: 'url("' + data.detail.posterUrl + '")',
+          backgroundImage: 'url("' + backgroundUrl + '")',
           height: "40vh",
         }}
       >
