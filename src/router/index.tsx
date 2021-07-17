2dc485cc03cab7198072d70d7b61507c0e494f3c
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-07-18 00:08:43 +0800

    Fixed typo

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -67,7 +67,7 @@ export const AppRouter: React.FC = () => {
         <Route exact path="/notab" component={NoTabPage} />
         <Route path="/tabs" component={TabsRoute} />
         <Route path="/signup" component={SignUp} />
-        <Route path="/org/:ordId/detail" component={OrgDetail} />
+        <Route path="/org/:orgId/detail" component={OrgDetail} />
         <Route path="/org/:ordId/apply" component={OrgApply} />
         <Route path="/org/:orgId/members" component={MemberManagement} />
         <Route path="/org/:orgId/home" component={OrgHome} />
