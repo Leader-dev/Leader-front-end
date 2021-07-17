258b32f5795f43ea1a2e662d636a2382070c1ec1
astatine-213-tian <318145208@qq.com>
2021-07-18 00:14:02 +0800

    Use api to fetch data Bug fixed

--- src/pages/Tabs/OrgDisplay/index.tsx
+++ src/pages/Tabs/OrgDisplay/index.tsx
@@ -10,113 +10,105 @@ import {
   IonIcon,
 } from "@ionic/react";
 import "./index.css";
-import { filterCircleSharp, filterOutline } from "ionicons/icons";
+import { filterOutline } from "ionicons/icons";
 import TopAdvertisement from "./Component/TopAdvertisement";
 import TopAdvertisementSkeleton from "./Component/TopAdvertisementSkeleton";
-import { RefresherEventDetail } from "@ionic/core";
 import RecommendOrganization from "./Component/RecommendOrganization";
 import RecommendOrganizationSkeleton from "./Component/RecommendOrganizationSkeleton";
 import { useHomeOrg } from "@/services/org/home";
 
 export default () => {
-  // fetch data from back end
-  // const { data: data, error } = useHomeOrg();
-  // if (error) return <div>failed to load</div>
-  // if (!data) return (
-  //   <div> Skeleton </div>
-  // )
-
   // Test Data
-  let orgRecommend = [
-      {
-        id: "xxx",
-        posterUrl:
-          "https://tva1.sinaimg.cn/large/008i3skNgy1gqnjj5kw0mj306v0bc753.jpg",
-        name: "这是一个社团",
-        numberId: 400031,
-        memberCount: 35,
-        instituteName: "深圳国际交流学院",
-        typeAliases: [],
-      },
-      {
-        id: "xxx",
-        posterUrl:
-          "https://tva1.sinaimg.cn/large/008i3skNgy1gqnjj5kw0mj306v0bc753.jpg",
-        name: "这是一个社团",
-        numberId: 400032,
-        memberCount: 35,
-        instituteName: "深圳国际交流学院",
-        typeAliases: [],
-      },
-      {
-        id: "xxx",
-        posterUrl:
-          "https://tva1.sinaimg.cn/large/008i3skNgy1gqnjj5kw0mj306v0bc753.jpg",
-        name: "这是一个社团",
-        numberId: 400033,
-        memberCount: 35,
-        instituteName: "深圳国际交流学院",
-        typeAliases: [],
-      },
-      {
-        id: "xxx",
-        posterUrl: "http://placekitten.com/g/200/300",
-        name: "这是一个社团",
-        numberId: 400034,
-        memberCount: 35,
-        instituteName: "深圳国际交流学院",
-        typeAliases: [],
-      },
-      {
-        id: "xxx",
-        posterUrl: "http://placekitten.com/g/200/300",
-        name: "这是一个社团",
-        numberId: 400035,
-        memberCount: 35,
-        instituteName: "深圳国际交流学院",
-        typeAliases: [],
-      },
-      {
-        id: "xxx",
-        posterUrl: "http://placekitten.com/g/200/300",
-        name: "这是一个社团",
-        numberId: 400036,
-        memberCount: 35,
-        instituteName: "深圳国际交流学院",
-        typeAliases: [],
-      },
-    ],
-    advertisement = [
-      {
-        id: "xxxxx",
-        posterUrl:
-          "https://tva1.sinaimg.cn/large/008i3skNgy1gr2n1qv6o5j30zk0mugx5.jpg",
-      },
-      {
-        id: "xxxxx",
-        posterUrl:
-          "https://tva1.sinaimg.cn/large/008i3skNgy1gr2n1qv6o5j30zk0mugx5.jpg",
-      },
-    ];
+  // let orgRecommend = [
+  //     {
+  //       id: "xxx",
+  //       posterUrl:
+  //         "https://tva1.sinaimg.cn/large/008i3skNgy1gqnjj5kw0mj306v0bc753.jpg",
+  //       name: "这是一个社团",
+  //       numberId: 400031,
+  //       memberCount: 35,
+  //       instituteName: "深圳国际交流学院",
+  //       instituteAuth: "school",
+  //       typeAliases: [],
+  //     },
+  //     {
+  //       id: "xxx",
+  //       posterUrl:
+  //         "https://tva1.sinaimg.cn/large/008i3skNgy1gqnjj5kw0mj306v0bc753.jpg",
+  //       name: "这是一个社团",
+  //       numberId: 400032,
+  //       memberCount: 35,
+  //       instituteName: "深圳国际交流学院",
+  //       instituteAuth: "school",
+  //       typeAliases: [],
+  //     },
+  //     {
+  //       id: "xxx",
+  //       posterUrl:
+  //         "https://tva1.sinaimg.cn/large/008i3skNgy1gqnjj5kw0mj306v0bc753.jpg",
+  //       name: "这是一个社团",
+  //       numberId: 400033,
+  //       memberCount: 35,
+  //       instituteName: "深圳国际交流学院",
+  //       instituteAuth: "school",
+  //       typeAliases: [],
+  //     },
+  //     {
+  //       id: "xxx",
+  //       posterUrl: "http://placekitten.com/g/200/300",
+  //       name: "这是一个社团",
+  //       numberId: 400034,
+  //       memberCount: 35,
+  //       instituteName: "深圳国际交流学院",
+  //       instituteAuth: "school",
+  //       typeAliases: [],
+  //     },
+  //     {
+  //       id: "xxx",
+  //       posterUrl: "http://placekitten.com/g/200/300",
+  //       name: "这是一个社团",
+  //       numberId: 400035,
+  //       memberCount: 35,
+  //       instituteName: "深圳国际交流学院",
+  //       instituteAuth: "school",
+  //       typeAliases: [],
+  //     },
+  //     {
+  //       id: "xxx",
+  //       posterUrl: "http://placekitten.com/g/200/300",
+  //       name: "这是一个社团",
+  //       numberId: 400036,
+  //       memberCount: 35,
+  //       instituteName: "深圳国际交流学院",
+  //       instituteAuth: "school",
+  //       typeAliases: [],
+  //     },
+  //   ],
+  //   advertisement = [
+  //     {
+  //       id: "xxxxx",
+  //       posterUrl:
+  //         "https://tva1.sinaimg.cn/large/008i3skNgy1gr2n1qv6o5j30zk0mugx5.jpg",
+  //     },
+  //     {
+  //       id: "xxxxx",
+  //       posterUrl:
+  //         "https://tva1.sinaimg.cn/large/008i3skNgy1gr2n1qv6o5j30zk0mugx5.jpg",
+  //     },
+  //   ];
+
+  // fetch data from back end
+  const { data: homeInfo, error } = useHomeOrg();
+  if (error) return <div>failed to load</div>;
 
   let adList, orgList;
-  let loadingFirst = false;
-  let tabBarHeight;
-  if (loadingFirst) {
-    tabBarHeight = "8vh";
+  if (!homeInfo) {
+    // debugger
     adList = <TopAdvertisementSkeleton />;
-    orgList = <RecommendOrganizationSkeleton tabBarHeight={tabBarHeight} />;
+    orgList = <RecommendOrganizationSkeleton />;
   } else {
-    tabBarHeight = document.getElementsByTagName("ion-tab-bar")[0].clientHeight;
-    if (tabBarHeight === 0) {
-      tabBarHeight = "8vh";
-    } else {
-      tabBarHeight = tabBarHeight + "px";
-    }
-    adList = <TopAdvertisement info={advertisement} />;
-    orgList = (
-      <RecommendOrganization info={orgRecommend} tabBarHeight={tabBarHeight} />
-    );
+    adList = <TopAdvertisement info={homeInfo.pic} />;
+    orgList = <RecommendOrganization info={homeInfo.list} />;
   }
 
   return (
