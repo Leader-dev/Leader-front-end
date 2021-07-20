21a760de66961791a21e3cb5c933e2fb4422b7c9
JokeNeverSoke <zengjoseph@hotmail.com>
2021-07-21 01:26:04 +0800

    create basic recruit manage page with mock data

---
+++ src/services/org/manage/apply/detail.ts
@@ -0,0 +1,41 @@
+import { ApplicationForm, Timestamp, UserOverview } from "@/types/organization";
+import axios from "@/utils/request";
+import useSWR from "swr";
+
+interface UseOrgApplicationDetail {
+  id: string;
+  name: string;
+  sendDate: Timestamp;
+  status: "pending" | "accepted" | "declined";
+  applicationForm: ApplicationForm;
+  applicantUserId: string;
+  applicantUserInfo: UserOverview;
+  departmentId: string;
+  departmentInfo: { id: string; name: string };
+  notifications: Array<{
+    id: string;
+    applicantId: string;
+    title: string;
+    content: string;
+    imageUrls: string[];
+    unread: boolean;
+    sendDate: Timestamp;
+  }>;
+}
+
+export const useOrgApplicationDetail = ({
+  orgId,
+  applicationId,
+}: {
+  orgId: string;
+  applicationId: string;
+}) => {
+  return useSWR(
+    [`/org/manage/apply/detail?orgId=${orgId}`, applicationId],
+    (a, d) => {
+      return axios
+        .post(a, { applicationId: d })
+        .then((res) => res.data.detail as UseOrgApplicationDetail);
+    }
+  );
+};
