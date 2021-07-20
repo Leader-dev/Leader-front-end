1dc0fb7eda9278f04bf2051243f25bb8286818e4
astatine-213-tian <318145208@qq.com>
2021-07-20 14:33:57 +0800

    Add routing to org detail page

--- src/pages/Tabs/OrgDisplay/Component/TopAdvertisement.tsx
+++ src/pages/Tabs/OrgDisplay/Component/TopAdvertisement.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import { IonSlides, IonSlide, IonImg } from "@ionic/react";
 import { AdInfo } from "@/types/organization";
+import { useHistory } from "react-router";
 
 export default ({ info }: { info: AdInfo[] }) => {
+  const history = useHistory();
   let advertisementList = info.map((info) => (
     <IonSlide
       style={{
         borderRadius: "10px",
         overflow: "hidden",
+      }}
+      onClick={() => {
+        history.push({
+          pathname: `/org/${info.id}/detail`,
+        });
       }}
     >
       <IonImg
@@ -18,7 +25,6 @@ export default ({ info }: { info: AdInfo[] }) => {
           objectFit: "cover",
         }}
       />
-      {/*TODO Onclick function*/}
     </IonSlide>
   ));
   return (
