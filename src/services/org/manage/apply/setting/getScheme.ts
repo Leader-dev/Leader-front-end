b3f4928df169f0796698e0564aff907b5b80fa6a
astatine-213-tian <318145208@qq.com>
2021-07-27 21:34:08 +0800

    Change interface name

--- src/services/org/manage/apply/setting/getScheme.ts
+++ src/services/org/manage/apply/setting/getScheme.ts
@@ -1,20 +1,20 @@
 import axios from "@/utils/request";
 import useSWR from "swr";
 
-interface OrgApplicationSetting {
+interface OrgRecruitSetting {
   open: boolean;
   maximumApplication: number;
   appointDepartment: number;
   questions: { questions: string; required: boolean };
 }
 
-interface useOrgApplicationSetting {
-  scheme: OrgApplicationSetting;
+interface useOrgRecruitSetting {
+  scheme: OrgRecruitSetting;
   receivedApplicationCount: number;
 }
 
 export const useOrgApplicationSetting = ({ orgId }: { orgId: string }) => {
   return useSWR(`/org/manage/apply/setting/get-scheme?orgId=${orgId}`, (url) =>
-    axios(url).then((res) => res.data.data as useOrgApplicationSetting)
+    axios(url).then((res) => res.data.data as useOrgRecruitSetting)
   );
 };
