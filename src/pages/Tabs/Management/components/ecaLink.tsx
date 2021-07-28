fa023c4571da91c9949fef4e83efb918bcfa2245
astatine-213-tian <318145208@qq.com>
2021-07-28 22:04:46 +0800

    Fix UI

--- src/pages/Tabs/Management/components/ecaLink.tsx
+++ src/pages/Tabs/Management/components/ecaLink.tsx
@@ -22,84 +22,79 @@ interface ECACardInfo extends OrgInfo {
 
 interface ECARequestCardInfo extends OrgInfo {
   notificationCount: number;
-  status: "pending" | "rejected" | "accepted";
+  status: string;
 }
 
 export const ECACard = ({ info }: { info: ECACardInfo }) => {
   const { data: startUrl } = useStartUrl();
   return (
     <IonCard style={{ margin: "10px 0" }}>
-      <IonCardContent>
-        <IonGrid>
-          <IonRow>
-            <IonCol size="5">
-              <IonImg // TODO: Fix aspect ratio & height problems
-                style={{
-                  borderRadius: "8px",
-                  width: "100%",
-                  height: "100%",
-                  maxHeight: "10vh",
-                  objectFit: "cover",
-                  overflow: "hidden",
-                }}
-                src={startUrl + info.posterUrl}
-              />
-            </IonCol>
-            <IonCol
-              className="ion-align-self-center"
-              size="5"
-              style={{ fontSize: "80%", color: "black" }}
+      <IonGrid style={{ padding: "12px" }}>
+        <IonRow>
+          <IonCol size="5">
+            <IonImg // TODO: Fix aspect ratio & height problems
+              style={{
+                borderRadius: "8px",
+                width: "100%",
+                height: "12vh",
+                objectFit: "cover",
+                overflow: "hidden",
+              }}
+              src={startUrl + info.posterUrl}
+            />
+          </IonCol>
+          <IonCol
+            className="ion-align-self-center"
+            size="4"
+            style={{ fontSize: "85%", color: "black", lineHeight: "150%" }}
+          >
+            <div style={{ fontSize: "120%", fontWeight: "bold" }}>
+              {info.name}
+            </div>
+            <div style={{ color: "var(--ion-color-medium)", fontSize: "100%" }}>
+              {info.numberId}
+            </div>
+            <div style={{ color: "var(--ion-color-primary" }}>
+              {info.instituteName}
+            </div>
+            <div>
+              负责人:{" "}
+              <span style={{ color: "var(--ion-color-primary)" }}>
+                {info.presidentName}
+              </span>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                color: "var(--ion-color-primary)",
+              }}
             >
-              <div style={{ fontSize: "120%", fontWeight: "bold" }}>
-                {info.name}
-              </div>
-              <div
-                style={{ color: "var(--ion-color-medium)", fontSize: "100%" }}
-              >
-                {info.numberId}
-              </div>
-              <div style={{ color: "var(--ion-color-primary" }}>
-                {info.instituteName}
-              </div>
-              <div>
-                负责人:{" "}
-                <span style={{ color: "var(--ion-color-primary)" }}>
-                  {info.presidentName}
-                </span>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  color: "var(--ion-color-primary)",
-                }}
-              >
-                <IonIcon icon={peopleSharp} style={{ marginRight: "2px" }} />
-                成员数 {info.memberCount}
-              </div>
-            </IonCol>
-            <IonCol
-              className="ion-align-self-center"
-              size="2"
-              style={{ textAlign: "right" }}
+              <IonIcon icon={peopleSharp} style={{ marginRight: "2px" }} />
+              成员数 {info.memberCount}
+            </div>
+          </IonCol>
+          <IonCol
+            className="ion-align-self-center"
+            size="3"
+            style={{ textAlign: "right" }}
+          >
+            {/*<div>*/}
+            {/*  {info.notificationCount && (*/}
+            {/*    <IonBadge color="danger">{info.notificationCount}</IonBadge>*/}
+            {/*  )}*/}
+            {/*</div>*/}
+            <IonButton
+              fill="outline"
+              size="small"
+              routerLink={`/org/${info.id}/home`}
+              style={{ "--border-radius": "8px" }}
             >
-              {/*<div>*/}
-              {/*  {info.notificationCount && (*/}
-              {/*    <IonBadge color="danger">{info.notificationCount}</IonBadge>*/}
-              {/*  )}*/}
-              {/*</div>*/}
-              <IonButton
-                fill="outline"
-                size="small"
-                routerLink={`/org/${info.id}/home`}
-                style={{ "--border-radius": "8px" }}
-              >
-                点击管理
-              </IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </IonCardContent>
+              点击管理
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
     </IonCard>
   );
 };
@@ -125,7 +120,7 @@ export const ECARequestCard = ({ info }: { info: ECARequestCardInfo }) => {
           <IonCol
             className="ion-align-self-center"
             size="4"
-            style={{ fontSize: "80%", color: "black", lineHeight: "150%" }}
+            style={{ fontSize: "85%", color: "black", lineHeight: "160%" }}
           >
             <div style={{ fontSize: "120%", fontWeight: "bold" }}>
               {info.name}
