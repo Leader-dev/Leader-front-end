ac1e78b2c5eb15ce531df6bdeac6900ff880696f
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-07-28 16:46:43 +0800

    Bug fixed

--- src/services/org/manage/apply/setting/getScheme.ts
+++ src/services/org/manage/apply/setting/getScheme.ts
@@ -1,17 +1,6 @@
 import axios from "@/utils/request";
 import useSWR from "swr";
-
-interface OrgRecruitSetting {
-  open: boolean;
-  maximumApplication: number;
-  appointDepartment: number;
-  questions: { questions: string; required: boolean };
-}
-
-interface useOrgRecruitSetting {
-  scheme: OrgRecruitSetting;
-  receivedApplicationCount: number;
-}
+import { useOrgRecruitSetting } from "@/types/recruit";
 
 export const useOrgApplicationSetting = ({ orgId }: { orgId: string }) => {
   return useSWR(`/org/manage/apply/setting/get-scheme?orgId=${orgId}`, (url) =>
