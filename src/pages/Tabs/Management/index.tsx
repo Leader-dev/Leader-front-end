7357dc88bab3ae049621185c22c3dfa71828afe7
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-07-28 16:02:01 +0800

    UI Enhancement

--- src/pages/Tabs/Management/index.tsx
+++ src/pages/Tabs/Management/index.tsx
@@ -52,97 +52,33 @@ const Management: React.FC = () => {
           </IonSegment>
         }
       >
-        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton routerLink="/org/create">
             <IonIcon icon={add} />
           </IonFabButton>
         </IonFab>
         {tab === "joined" ? (
           loading ? null : (
-            orgList?.map(
-              ({
-                name,
-                id,
-                numberId,
-                posterUrl,
-                addressAuth,
-                address,
-                memberCount,
-                presidentName,
-              }) => {
-                return (
-                  <ECACard
-                    name={name}
-                    numberId={numberId}
-                    imgUrl={posterUrl}
-                    addressAuth={addressAuth}
-                    address={address}
-                    memberCount={memberCount}
-                    presidentName={presidentName}
-                    notificationCount={0}
-                    id={id}
-                    key={id}
-                  />
-                );
-              }
-            )
+            orgList?.map((org) => {
+              return <ECACard info={org} key={org.id} />;
+            })
           )
         ) : (
           <>
             <ECARequestCard
-              name="Leader 开发组"
-              numberId={114514}
-              imgUrl="https://www.baidu.com/img/flexible/logo/pc/result.png"
-              addressAuth="school"
-              address="深圳国际交流学院"
-              memberCount={1919}
-              presidentName="米老鼠"
-              notificationCount={3}
-              status="pending"
-            />
-            <ECARequestCard
-              name="Leader 开发组"
-              numberId={114514}
-              imgUrl="https://www.baidu.com/img/flexible/logo/pc/result.png"
-              addressAuth="school"
-              address="深圳国际交流学院"
-              memberCount={1919}
-              presidentName="米老鼠"
-              notificationCount={3}
-              status="accepted"
-            />
-            <ECARequestCard
-              name="Leader 开发组"
-              numberId={114514}
-              imgUrl="https://www.baidu.com/img/flexible/logo/pc/result.png"
-              addressAuth="school"
-              address="深圳国际交流学院"
-              memberCount={1919}
-              presidentName="米老鼠"
-              notificationCount={3}
-              status="rejected"
-            />
-            <ECARequestCard
-              name="Leader 开发组"
-              numberId={114514}
-              imgUrl="https://www.baidu.com/img/flexible/logo/pc/result.png"
-              addressAuth="school"
-              address="深圳国际交流学院"
-              memberCount={1919}
-              presidentName="米老鼠"
-              notificationCount={3}
-              status="rejected"
-            />
-            <ECARequestCard
-              name="Leader 开发组"
-              numberId={114514}
-              imgUrl="https://www.baidu.com/img/flexible/logo/pc/result.png"
-              addressAuth="school"
-              address="深圳国际交流学院"
-              memberCount={1919}
-              presidentName="米老鼠"
-              notificationCount={3}
-              status="rejected"
+              info={{
+                id: "",
+                name: "Leader 开发组",
+                numberId: 114514,
+                posterUrl: "v1_9w8xDVon5GnbCHCAPxVLLWPPowdXILcJ",
+                instituteName: "深圳国际交流学院",
+                instituteAuth: "school",
+                memberCount: 1919,
+                presidentName: "米老鼠",
+                notificationCount: 3,
+                status: "pending",
+                typeAliases: [],
+              }}
             />
           </>
         )}
