e90bff758d1321a6434af4f9f1973dc6613376a8
astatine-213-tian <318145208@qq.com>
2021-07-28 22:05:38 +0800

    More recruit settings

--- src/pages/Org/Recruit/Settings/components/SettingForm.tsx
+++ src/pages/Org/Recruit/Settings/components/SettingForm.tsx
@@ -10,18 +10,26 @@ import {
 } from "@ionic/react";
 import { useState } from "react";
 import { chevronForward } from "ionicons/icons";
+import { useOrgRecruitSetting } from "@/types/recruit";
+import RecruitQuestions from "./RecruitQuestions";
 
-export default () => {
-  const [open, setOpen] = useState<boolean>();
-  const [maximumApplication, setMaximumApplication] = useState<number>();
-  const [questions, setQuestions] =
-    useState<{ questions: string; required: boolean }>();
+export default ({ recruitInfo }: { recruitInfo: useOrgRecruitSetting }) => {
+  const { scheme, receivedApplicationCount } = recruitInfo;
+  const [open, setOpen] = useState<boolean>(scheme.open);
+  const [maximumApplication, setMaximumApplication] = useState<number>(
+    scheme.maximumApplication
+  );
+  const [questions, setQuestions] = useState<
+    { question: string; required: boolean }[]
+  >(scheme.questions);
+  const [received, setReceived] = useState<number>(receivedApplicationCount);
+  let reset = false;
 
   return (
     <>
       <IonItem>
         <IonLabel>是否开启主页申请通道</IonLabel>
-        <IonToggle checked={open} />
+        <IonToggle checked={open} onIonChange={() => setOpen(open)} />
       </IonItem>
       <IonItem>
         <IonLabel>是否限制招新名额上限制</IonLabel>
@@ -29,18 +37,29 @@ export default () => {
       </IonItem>
       <IonItem>
         <IonLabel>招新人数上限：</IonLabel>
-        <IonInput value={maximumApplication} />
+        <IonInput
+          value={maximumApplication}
+          onIonChange={() => setMaximumApplication(maximumApplication)}
+        />
       </IonItem>
       <IonItem>
-        <IonLabel>已用招新名额：</IonLabel>
-        <IonInput value={maximumApplication} />
-        <IonButton>清零</IonButton>
+        <IonLabel>已用招新名额：{received}</IonLabel>
+        <IonButton
+          onClick={() => {
+            reset = true;
+            setReceived(0);
+          }}
+        >
+          清零
+        </IonButton>
       </IonItem>
 
       <h6>申请者审核:</h6>
       <IonItem>
         <IonLabel style={{ color: "primary" }}>您的姓名</IonLabel>
       </IonItem>
+
+      <RecruitQuestions questions={questions} setQuestions={setQuestions} />
 
       <IonLabel>添加各部门招新审核人</IonLabel>
       <IonButton fill="clear">
