a23e263559cb752219493fdae7d4639b5dc0c88e
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-07-30 14:47:33 +0800

    Add mutate

--- src/services/user/info/updateNickname.ts
+++ src/services/user/info/updateNickname.ts
@@ -1,5 +1,7 @@
 import axios from "@/utils/request";
+import { mutate } from "swr";
 
 export const updateUserNickName = async (nickname: string) => {
   await axios.post("/user/info/update-nickname", { nickname });
+  await mutate("/user/info/get");
 };
