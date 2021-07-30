6adc3426dbbd60ff1d3b894bbc8bbd6e8c3e4214
JokeNeverSoke <zengjoseph@hotmail.com>
2021-07-30 19:11:55 +0800

    add trend post detail page

--- src/pages/Tabs/Trends/Component/TopMomentItem.tsx
+++ src/pages/Tabs/Trends/Component/TopMomentItem.tsx
@@ -4,36 +4,36 @@ import User from "./User";
 import * as React from "react";
 import { MomentInfo } from "./MomentCard";
 
-export interface TopMomentInfo extends MomentInfo {
-  upRank: number;
-}
+// export interface TopMomentInfo extends MomentInfo {
+//   upRank: number;
+// }
 
-export default ({ info }: { info: TopMomentInfo }) => {
-  return (
-    <IonItem>
-      <div style={{ marginLeft: -4, marginRight: 12 }} slot="start">
-        <IonLabel>
-          <IonText color="primary">
-            <p>
-              <IonIcon slot="end" icon={arrowUp} />#{info.upRank}
-            </p>
-          </IonText>
-        </IonLabel>
-      </div>
-      <User info={info} />
-      <IonLabel>
-        <p>{info.content}</p>
-      </IonLabel>
-      <div slot="end">
-        <IonLabel>
-          <IonText color="primary">
-            <p>
-              <IonIcon slot="end" icon={arrowUp} />
-              {info.upCount}
-            </p>
-          </IonText>
-        </IonLabel>
-      </div>
-    </IonItem>
-  );
-};
+// export default ({ info }: { info: TopMomentInfo }) => {
+//   return (
+//     <IonItem>
+//       {/* <div style={{ marginLeft: -4, marginRight: 12 }} slot="start">
+//         <IonLabel>
+//           <IonText color="primary">
+//             <p>
+//               <IonIcon slot="end" icon={arrowUp} />#{info.upRank}
+//             </p>
+//           </IonText>
+//         </IonLabel>
+//       </div>
+//       <User info={info} />
+//       <IonLabel>
+//         <p>{info.content}</p>
+//       </IonLabel>
+//       <div slot="end">
+//         <IonLabel>
+//           <IonText color="primary">
+//             <p>
+//               <IonIcon slot="end" icon={arrowUp} />
+//               {info.upCount}
+//             </p>
+//           </IonText>
+//         </IonLabel>
+//       </div> */}
+//     </IonItem>
+//   );
+// };
