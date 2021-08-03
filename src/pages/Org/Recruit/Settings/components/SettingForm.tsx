e9c36433403a130eeb1cdbe4c09dd6e46a3ca1bd
astatine-213-tian <318145208@qq.com>
2021-08-03 14:39:45 +0800

    Add submit

--- src/pages/Org/Recruit/Settings/components/SettingForm.tsx
+++ src/pages/Org/Recruit/Settings/components/SettingForm.tsx
@@ -4,33 +4,42 @@ import {
   IonIcon,
   IonInput,
   IonItem,
+  IonList,
   IonLabel,
   IonListHeader,
   IonNote,
   IonToggle,
+  useIonRouter,
 } from "@ionic/react";
 import { useState } from "react";
 import { checkmarkCircle, chevronForward } from "ionicons/icons";
 import { useOrgRecruitSetting } from "@/types/recruit";
 import RecruitQuestions from "./RecruitQuestions";
+import { setOrgRecruitSetting } from "@/services/org/manage/apply/setting/setScheme";
+import { useParams } from "react-router";
 
 export default ({ recruitInfo }: { recruitInfo: useOrgRecruitSetting }) => {
+  const { orgId } = useParams<{ orgId: string }>();
   const { scheme, receivedApplicationCount } = recruitInfo;
   const [open, setOpen] = useState<boolean>(scheme.open);
   const [maximumApplication, setMaximumApplication] = useState<number>(
-    scheme.maximumApplication
+    scheme.maximumApplication === -1 ? 0 : scheme.maximumApplication
   );
   const [limitChecked, setLimitChecked] = useState<boolean>(
-    maximumApplication !== -1
+    scheme.maximumApplication !== -1
   );
   const [questions, setQuestions] = useState<
     { question: string; required: boolean }[]
   >(scheme.questions);
   const [received, setReceived] = useState<number>(receivedApplicationCount);
-  let reset = false;
+  const [appointDepartment, setAppointDepartment] = useState<boolean>(
+    scheme.appointDepartment
+  );
+
+  const history = useIonRouter();
 
   return (
-    <form>
+    <IonList>
       <IonItem>
         <IonLabel>是否开启主页申请通道</IonLabel>
         <IonNote slot={"end"}>
@@ -44,42 +53,43 @@ export default ({ recruitInfo }: { recruitInfo: useOrgRecruitSetting }) => {
             <IonNote slot={"end"}>
               <IonToggle
                 checked={limitChecked}
-                onIonChange={(e) => setLimitChecked(e.detail.checked)}
+                onIonChange={(e) => {
+                  setLimitChecked(e.detail.checked);
+                }}
               />
             </IonNote>
           </IonItem>
           {limitChecked ? (
-            <IonItem>
-              <IonLabel>招新人数上限：</IonLabel>
-              <IonInput
-                style={{ marginLeft: "-7px" }}
-                value={maximumApplication}
-                onIonChange={() => setMaximumApplication(maximumApplication)}
-              />
-            </IonItem>
+            <>
+              <IonItem>
+                <IonLabel>招新人数上限：</IonLabel>
+                <IonInput
+                  style={{ marginLeft: "-7px" }}
+                  value={maximumApplication}
+                  inputmode={"numeric"}
+                  type={"number"}
+                  onIonChange={(e) =>
+                    setMaximumApplication(parseInt(e.detail.value!))
+                  }
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel>已用招新名额：{received}</IonLabel>
+                <IonNote slot="end">
+                  <IonButton
+                    color="danger"
+                    onClick={() => {
+                      setReceived(0);
+                    }}
+                  >
+                    清零
+                  </IonButton>
+                </IonNote>
+              </IonItem>
+            </>
           ) : (
-            <IonItem>
-              <IonLabel>招新人数上限：</IonLabel>
-              <IonInput
-                style={{ marginLeft: "-7px" }}
-                value={maximumApplication}
-              />
-            </IonItem>
+            ""
           )}
-          <IonItem>
-            <IonLabel color={"medium"}>已用招新名额：{received}</IonLabel>
-            <IonNote slot="end">
-              <IonButton
-                color="danger"
-                onClick={() => {
-                  reset = true;
-                  setReceived(0);
-                }}
-              >
-                清零
-              </IonButton>
-            </IonNote>
-          </IonItem>
 
           <IonListHeader style={{ marginTop: 20, marginBottom: 0 }}>
             <h5>申请者审核:</h5>
@@ -105,17 +115,46 @@ export default ({ recruitInfo }: { recruitInfo: useOrgRecruitSetting }) => {
 
           <RecruitQuestions questions={questions} setQuestions={setQuestions} />
 
-          <IonItem>
-            <IonLabel>添加各部门招新审核人</IonLabel>
-            <IonButton fill="clear" routerLink={"departments"}>
-              点击设置
-              <IonIcon icon={chevronForward} />
-            </IonButton>
+          <IonItem style={{ marginTop: "5px" }}>
+            <IonLabel>申请者是否需要选择部门</IonLabel>
+            <IonNote slot={"end"}>
+              <IonToggle
+                checked={appointDepartment}
+                onIonChange={(e) => {
+                  setAppointDepartment(e.detail.checked);
+                }}
+              />
+            </IonNote>
           </IonItem>
+
+          {appointDepartment ? (
+            <IonItem>
+              <IonLabel>添加各部门招新审核人</IonLabel>
+              <IonButton fill="clear" routerLink={"departments"}>
+                点击设置
+                <IonIcon icon={chevronForward} />
+              </IonButton>
+            </IonItem>
+          ) : (
+            ""
+          )}
+
           <IonButton
             style={{ margin: "25px 15px" }}
             expand="block"
-            type="submit"
+            onClick={() => {
+              setOrgRecruitSetting({
+                orgId: orgId,
+                scheme: {
+                  open: open,
+                  maximumApplication: limitChecked ? maximumApplication : -1,
+                  appointDepartment: appointDepartment,
+                  questions: questions,
+                },
+                resetReceivedApplicationCount: received === 0,
+              });
+              history.goBack();
+            }}
           >
             确认
           </IonButton>
@@ -123,6 +162,6 @@ export default ({ recruitInfo }: { recruitInfo: useOrgRecruitSetting }) => {
       ) : (
         ""
       )}
-    </form>
+    </IonList>
   );
 };
