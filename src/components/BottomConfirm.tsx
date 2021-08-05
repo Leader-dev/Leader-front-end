23e37810c089bcf0128dbb53576537751764fb6c
astatine-213-tian <318145208@qq.com>
2021-08-05 18:22:54 +0800

    Create a component for bottom confirm button

---
+++ src/components/BottomConfirm.tsx
@@ -0,0 +1,45 @@
+import { IonButton } from "@ionic/react";
+import * as React from "react";
+
+export default ({
+  title,
+  submit,
+  onClick,
+}: {
+  title: string;
+  submit: boolean;
+  onClick?: any;
+}) => {
+  return (
+    <>
+      <div style={{ height: "10vh" }} />
+      <div
+        style={{
+          position: "fixed",
+          width: "100%",
+          bottom: 0,
+          display: "flex",
+          background: "white",
+        }}
+      >
+        {submit ? (
+          <IonButton
+            style={{ margin: "15px 5vw", width: "100%" }}
+            expand={"block"}
+            type={"submit"}
+            onClick={onClick}
+          >
+            {title}
+          </IonButton>
+        ) : (
+          <IonButton
+            style={{ margin: "15px 5vw", width: "100%" }}
+            expand={"block"}
+          >
+            {title}
+          </IonButton>
+        )}
+      </div>
+    </>
+  );
+};
