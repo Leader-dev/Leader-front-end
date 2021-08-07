90bb27a2fca88e773a4f8189db546e0ecb69584e
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-08-07 11:28:43 +0800

    Add more styling options

--- src/components/ToolbarWithBackButton.tsx
+++ src/components/ToolbarWithBackButton.tsx
@@ -2,11 +2,9 @@ import * as React from "react";
 import { IonButtons, IonTitle, IonToolbar, IonBackButton } from "@ionic/react";
 import { chevronBack } from "ionicons/icons";
 
-export const ToolbarWithBackButton: React.FC<{ title: string }> = ({
-  title,
-}) => {
+export default ({ title, border }: { title: string; border: boolean }) => {
   return (
-    <IonToolbar>
+    <IonToolbar style={{ "--border-style": border ? "solid" : "none" }}>
       <IonButtons>
         <IonBackButton text="" icon={chevronBack} defaultHref="/" />
       </IonButtons>
