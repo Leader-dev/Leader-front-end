93c9cf8e8b64f3bad0c07d6d9d78f6a9fc7231b9
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-08-07 11:28:23 +0800

    UI Enhancement

--- src/pages/Org/Detail/index.tsx
+++ src/pages/Org/Detail/index.tsx
@@ -10,7 +10,7 @@ import {
   IonIcon,
   IonSegment,
   IonSegmentButton,
-  IonBackButton,
+  useIonRouter,
 } from "@ionic/react";
 import "./index.css";
 import {
@@ -46,48 +46,10 @@ export default () => {
   let orgContent, backgroundUrl;
   if (detailError) {
     // Test data
-    let testData = {
-      detail: {
-        id: "xxxxxx",
-        name: "计算机协会",
-        numberId: 111111,
-        introduction:
-          "计算机协会成⽴于2004年10⽉10⽇，是学院最具潜⼒的学⽣社团之⼀，也是学院唯⼀⼀个科技类社团。协会是由⼴⼤的电脑爱好者⾃发组成，以“学习计算机知识，提⾼⾃⾝素质，相互帮助，团结协作”为宗旨，以“先进带动后进， 刻苦钻研计算机知识，勇攀IT科技⾼峰”为原则。积极⼤胆地⾛进计算机各个领域，不断总结交流，树⽴良好的计算机知识氛围，全⾯提⾼我院⼴⼤计算机爱好者们的计算机知识⽔平。",
-        memberCount: 280,
-        instituteName: "深国交",
-        address: "广东深圳",
-        instituteAuth: "school",
-        typeAliases: [""],
-        posterUrl:
-          "https://tva1.sinaimg.cn/large/008i3skNgy1gsbsxdsc4ij30a006rjrp.jpg",
-        status: "",
-        phone: ["113115155", "189237523465"],
-        email: ["aaaaa@gmail.com", "test@qq.com"],
-        presidentName: "正在讲",
-        applicationScheme: {
-          open: false,
-          auth: true,
-          appointDepartment: true,
-          questions: [
-            { question: "请做一个自我介绍", required: true },
-            { question: "为什么想来我们的社团", required: false },
-          ],
-        },
-      },
-      applicationStatus: "available",
-      favorite: true,
-    };
-    orgContent =
-      tab === "info" ? (
-        <OrgDetailInfo info={testData} />
-      ) : (
-        <OrgDetailContact info={testData} />
-      );
-    backgroundUrl = testData.detail.posterUrl;
+    orgContent = <div>Error</div>;
   } else {
     if (!orgDetail) {
       orgContent = <InfoSkeleton />;
-      backgroundUrl = "";
     } else {
       orgContent =
         tab === "info" ? (
@@ -95,49 +57,54 @@ export default () => {
         ) : (
           <OrgDetailContact info={orgDetail} />
         );
-      backgroundUrl = orgDetail.detail.posterUrl;
     }
   }
 
   const handleFavorite = () => {
     if (favorite) {
       console.log("removing");
-      removeOrgFromFavorite(orgId).then((r) => setFavorite(false));
+      removeOrgFromFavorite(orgId).then(() => setFavorite(false));
     } else {
       console.log("adding");
-      addOrgToFavorite(orgId).then((r) => setFavorite(true));
+      addOrgToFavorite(orgId).then(() => setFavorite(true));
     }
   };
 
-  const startUrl = useStartUrl();
+  const { data: startUrl } = useStartUrl();
+  const history = useIonRouter();
 
   return (
     <IonPage>
-      <IonHeader
-        style={{
-          backgroundImage: 'url("' + startUrl + backgroundUrl + '")',
-          height: "80vw",
-        }}
-      >
-        <div
-          style={{
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-          }}
-        >
-          <IonToolbar className="transparent-toolbar">
+      <IonHeader style={{ height: "80vw" }}>
+        <div style={{ color: "white", position: "relative", height: "100%" }}>
+          <img
+            src={`${startUrl}${
+              orgDetail?.detail.posterUrl ||
+              "v1_ipNqlAbA7NJpPjS8ay2KRiwKeoSuTz4h"
+            }`}
+            style={{
+              top: 0,
+              zIndex: -1,
+              position: "absolute",
+              width: "100vw",
+              height: "80vw",
+              objectFit: "cover",
+            }}
+          />
+          <IonToolbar
+            style={{ "--background": "none", "--border-style": "none" }}
+          >
             <IonButtons slot="start">
-              <IonBackButton
+              <IonButton
+                shape={"round"}
                 style={{
                   "--background": "rgba(173,173,173,0.19)",
                   "--color": "#FFFFFF",
-                  "--border-radius": "50%",
                 }}
-                icon={chevronBack}
-                text=""
-              />
+                onClick={() => history.goBack()}
+              >
+                <IonIcon slot={"icon-only"} icon={chevronBack} />
+              </IonButton>
             </IonButtons>
             <IonButtons slot="end">
               <IonButton
@@ -165,13 +132,22 @@ export default () => {
             </IonButtons>
           </IonToolbar>
 
-          <IonToolbar className="rounded-toolbar">
+          <IonToolbar
+            className={"ion-no-padding"}
+            style={{
+              "--border-style": "none",
+              "border-radius": "18px 18px 0 0",
+              position: "absolute",
+              bottom: "0",
+            }}
+          >
             <IonSegment
               mode="md"
               value={tab}
               onIonChange={(e) => {
                 setTab(e.detail.value as "info" | "contact");
               }}
+              style={{ "--background": "none" }}
             >
               <IonSegmentButton value="info">
                 <IonLabel style={{ fontSize: "120%" }}> 详细信息 </IonLabel>
