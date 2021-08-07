a96db74877a1e7cf5b30928543bb7025177d1469
astatine-213-tian <318145208@qq.com>
2021-08-07 11:29:22 +0800

    Bug fixed. Complete Recruit settings.

--- src/pages/Org/Recruit/Settings/components/DepartmentSettings.tsx
+++ src/pages/Org/Recruit/Settings/components/DepartmentSettings.tsx
@@ -5,77 +5,50 @@ import {
   IonHeader,
   IonItem,
   IonLabel,
+  IonList,
+  IonListHeader,
   IonPage,
   useIonRouter,
 } from "@ionic/react";
-import { ToolbarWithBackButton } from "@/components/ToolbarWithBackButton";
+import ToolbarWithBackButton from "@/components/ToolbarWithBackButton";
 import { useParams } from "react-router";
 import { useRecruitManagerInfo } from "@/services/org/manage/apply/setting/getRecruitManagerInfo";
-import { OrgMember } from "@/types/organization";
-import { useEffect, useState } from "react";
-import { setRecruitMangerInfo } from "@/services/org/manage/apply/setting/setRecruitMangerInfo";
-import SelectMembers from "@/pages/Org/components/SelectMembers";
 
 export default () => {
   const { orgId } = useParams<{ orgId: string }>();
   const { data: rootManagerInfo, error: rootMangerInfoError } =
     useRecruitManagerInfo({ orgId: orgId, departmentId: null });
-  const [selectedMembers, setSelectedMembers] = useState<OrgMember[]>([]);
-  const [currDepartment, setCurrentDepartment] =
-    useState<{ id: string; name: string } | undefined>(undefined);
 
-  const history = useIonRouter();
-
-  useEffect(() => {
-    if (rootManagerInfo) {
-      setSelectedMembers([rootManagerInfo.manager]);
-    }
-  });
-
-  const handleOnSubmit = (departmentId: string) => {
-    setRecruitMangerInfo({
-      orgId: orgId,
-      departmentId: departmentId,
-      memberId: selectedMembers[0].id,
-    }).then(() => history.goBack());
-  };
-
+  let content;
   if (rootManagerInfo) {
-    if (!currDepartment) {
-      return (
-        <IonPage>
-          <IonHeader>
-            <ToolbarWithBackButton title={"添加各部门招新审核人"} />
-          </IonHeader>
-          <IonContent fullscreen>
-            {rootManagerInfo.departments?.map((department) => (
-              <IonItem button onClick={() => setCurrentDepartment(department)}>
-                <IonLabel>{department.name}</IonLabel>
-                <IonBadge slot={"end"} color={"primary"}>
-                  {department.recruitManagerCount}/1
-                </IonBadge>
-              </IonItem>
-            ))}
-          </IonContent>
-        </IonPage>
-      );
-    } else {
-      return (
-        <SelectMembers
-          selectedMembers={selectedMembers}
-          setSelectedMembers={setSelectedMembers}
-          limit={true}
-          onSubmit={() => handleOnSubmit(currDepartment.id)}
-          onBack={() => setCurrentDepartment(undefined)}
-          title={"设置" + currDepartment.name + "招新管理员"}
-        />
-      );
-    }
-  } else {
-    return (
-      <IonPage>
-        <div>Loading</div>
-      </IonPage>
+    content = rootManagerInfo.departments.length ? (
+      rootManagerInfo.departments.map((department) => (
+        <IonItem
+          key={department.id}
+          button
+          routerLink={`departments/${department.id}`}
+        >
+          <IonLabel>{department.name}</IonLabel>
+          <IonBadge slot={"end"} color={"primary"}>
+            {department.recruitManagerCount}/1
+          </IonBadge>
+        </IonItem>
+      ))
+    ) : (
+      <IonListHeader>
+        <h6> 暂无部门 </h6>
+      </IonListHeader>
     );
+  } else {
+    content = <div>Skeleton</div>;
   }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <ToolbarWithBackButton title={"添加各部门招新审核人"} border={true} />
+      </IonHeader>
+      <IonContent fullscreen>{content}</IonContent>
+    </IonPage>
+  );
 };
