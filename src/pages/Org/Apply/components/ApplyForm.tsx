98ea867ef86b70e9a95a01f6a346b5671173eba2
astatine-213-tian <318145208@qq.com>
2021-08-09 14:27:26 +0800

    Add department selection check

--- src/pages/Org/Apply/components/ApplyForm.tsx
+++ src/pages/Org/Apply/components/ApplyForm.tsx
@@ -9,20 +9,18 @@ import {
   IonSelectOption,
   IonTextarea,
   isPlatform,
+  useIonRouter,
 } from "@ionic/react";
 import { OrgDetailsResult } from "@/types/organization";
 import { useState } from "react";
 import "./ApplyForm.css";
 import { applyToOrg } from "@/services/org/apply/send";
-import { useHistory } from "react-router";
+import { OrgDepartment } from "@/types/organization";
+import BottomConfirm from "@/components/BottomConfirm";
+import { useToast } from "@/utils/toast";
 
 const options = {
   cssClass: "department-select-option",
-};
-
-type Department = {
-  id: string | null;
-  name: string | null;
 };
 
 export default ({
@@ -30,20 +28,20 @@ export default ({
   departments,
 }: {
   details: OrgDetailsResult;
-  departments: Department[];
+  departments: OrgDepartment[];
 }) => {
-  const history = useHistory();
+  const history = useIonRouter();
 
   const { detail } = details;
   const applicationInfo = detail.applicationScheme;
 
   const [name, setName] = useState<string>();
-  const [selectedDepartment, setSelectedDepartment] = useState<Department>({
-    id: null,
-    name: null,
-  });
+  const [selectedDepartment, setSelectedDepartment] =
+    useState<OrgDepartment | null>(null);
   const questions = applicationInfo.questions;
-  const [answers, setAnswers] = useState<string[]>([]);
+  const [answers, setAnswers] = useState<string[]>(
+    questions ? Array(questions.length).fill(null) : []
+  );
 
   let fontSize = isPlatform("ios") ? "90%" : "20px";
 
@@ -53,90 +51,28 @@ export default ({
     setAnswers(newAnswers);
   };
 
-  let inputList = questions
-    ? questions.map((q, index) => {
-        let required = q.required;
-        return (
-          <IonItem>
-            <IonLabel position="stacked" style={{ fontSize: fontSize }}>
-              {q.question}
-              {required ? (
-                <span
-                  style={{
-                    color: "var(--ion-color-primary)",
-                    marginLeft: "3px",
-                    fontWeight: "bold",
-                  }}
-                >
-                  *
-                </span>
-              ) : (
-                ""
-              )}
-            </IonLabel>
-            <IonTextarea
-              required={required}
-              rows={1}
-              autoGrow={true}
-              onIonChange={(e) => handleInputChange(e, index)}
-            />
-          </IonItem>
-        );
-      })
-    : null;
-
-  let departmentSelect;
-  if (applicationInfo.appointDepartment) {
-    let selectionOptions = departments.map((department) => (
-      <IonSelectOption key={department.id} value={department}>
-        {department.name}
-      </IonSelectOption>
-    ));
-    departmentSelect = (
-      <IonItem>
-        <IonLabel style={{ fontSize: "90%" }}> 希望加入的部门 </IonLabel>
-        <IonSelect
-          className="department-select"
-          interface="popover"
-          interfaceOptions={options}
-          placeholder="请选择"
-          value={selectedDepartment}
-          onIonChange={(e) => {
-            setSelectedDepartment(e.detail.value);
-          }}
-        >
-          {selectionOptions}
-        </IonSelect>
-      </IonItem>
-    );
-  } else {
-    departmentSelect = null;
-  }
+  const [present] = useToast();
 
   return (
     <form
       onSubmit={(event) => {
-        //   console.log({
-        //     orgId: detail.id,
-        //     departmentId: selectedDepartment.id,
-        //     applicationForm: answers.map((answer, index) => ({
-        //       question: questions[index].question,
-        //       answer: answer,
-        //     })),
-        //   });
-        //   history.goBack();
-        //   event.preventDefault();
-        // }
-        applyToOrg({
-          orgId: detail.id,
-          departmentId: selectedDepartment.id,
-          applicationForm: answers.map((answer, index) => ({
-            question: questions[index].question,
-            answer: answer,
-          })),
-        });
-        history.goBack();
         event.preventDefault();
+        if (!selectedDepartment) {
+          present({ message: "请选择部门" });
+        } else {
+          applyToOrg({
+            orgId: detail.id,
+            name: name!,
+            departmentId: selectedDepartment?.id || null,
+            applicationForm: questions
+              ? questions.map((question, index) => ({
+                  question: question.question,
+                  answer: answers[index],
+                }))
+              : [],
+          });
+          history.goBack();
+        }
       }}
     >
       <IonList>
@@ -159,12 +95,56 @@ export default ({
             onIonChange={(e) => setName(e.detail.value!)}
           />
         </IonItem>
-        {inputList}
-        {departmentSelect}
+        {questions
+          ? questions.map((q, index) => (
+              <IonItem>
+                <IonLabel position="stacked" style={{ fontSize: fontSize }}>
+                  {q.question}
+                  {q.required ? (
+                    <span
+                      style={{
+                        color: "var(--ion-color-primary)",
+                        marginLeft: "3px",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      *
+                    </span>
+                  ) : null}
+                </IonLabel>
+                <IonTextarea
+                  required={q.required}
+                  rows={1}
+                  autoGrow={true}
+                  onIonChange={(e) => handleInputChange(e, index)}
+                />
+              </IonItem>
+            ))
+          : null}
+
+        {applicationInfo.appointDepartment ? (
+          <IonItem>
+            <IonLabel style={{ fontSize: "90%" }}> 希望加入的部门 </IonLabel>
+            <IonSelect
+              className="department-select"
+              interface="popover"
+              interfaceOptions={options}
+              placeholder="请选择"
+              value={selectedDepartment}
+              onIonChange={(e) => {
+                setSelectedDepartment(e.detail.value);
+              }}
+            >
+              {departments.map((department) => (
+                <IonSelectOption key={department.id} value={department}>
+                  {department.name}
+                </IonSelectOption>
+              ))}
+            </IonSelect>
+          </IonItem>
+        ) : null}
       </IonList>
-      <IonButton style={{ margin: "25px 15px" }} expand="block" type="submit">
-        确认
-      </IonButton>
+      <BottomConfirm title={"确认提交"} submit={true} />
     </form>
   );
 };
