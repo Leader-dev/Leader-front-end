e13ad94fbf8fb7be81d49926d699fdfe64b1b84a
astatine-213-tian <318145208@qq.com>
2021-08-12 01:52:38 +0800

    Bug fixed

--- src/services/org/manage/memberInfo/resign.ts
+++ src/services/org/manage/memberInfo/resign.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from "@/utils/request";
 
-export const resignMember = async (orgId: strng) => {
+export const resignMember = async (orgId: string) => {
   await axios
     .post(`/org/manage/member-info/resign?orgId=${orgId}`)
     .then((r) => console.log(r));
