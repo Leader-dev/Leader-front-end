a836b1b91e8eaad8373e5f0e79f22b4ed01a4ac3
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-08-17 18:50:40 +0800

    Encapsulate component

--- src/pages/Org/components/MemberCard.tsx
+++ src/pages/Org/components/MemberCard.tsx
@@ -1,8 +1,44 @@
-import { IonIcon, IonItem, IonLabel, IonNote, IonText } from "@ionic/react";
+import {
+  IonIcon,
+  IonItem,
+  IonItemSliding,
+  IonLabel,
+  IonNote,
+  IonText,
+} from "@ionic/react";
 import { checkmarkCircle } from "ionicons/icons";
 import * as React from "react";
 import { OrgMember } from "@/types/organization";
 import UserAvatar from "@/components/UserAvatar";
+
+export const MemberCardWithSliding = ({
+  memberInfo,
+  routerLink,
+  itemOptions,
+}: {
+  memberInfo: OrgMember;
+  routerLink: string;
+  itemOptions: React.ReactNode;
+}) => {
+  return (
+    <IonItemSliding key={memberInfo.id}>
+      <IonItem detail={true} routerLink={routerLink}>
+        <UserAvatar src={memberInfo.avatarUrl!} />
+        <IonLabel>
+          <h2 style={{ lineHeight: "140%" }}>
+            {memberInfo.name!}
+            <IonText color={"primary"}>{memberInfo.title!}</IonText>
+          </h2>
+          <p style={{ fontSize: "70%" }}>
+            <IonText color={"primary"}>成员号:</IonText>
+            <span style={{ marginLeft: "2px" }}>{memberInfo.numberId!}</span>
+          </p>
+        </IonLabel>
+      </IonItem>
+      {itemOptions}
+    </IonItemSliding>
+  );
+};
 
 export default ({
   memberInfo,
