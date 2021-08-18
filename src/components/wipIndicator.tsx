03dbf1c34839e8462583c68fba36c0e7a268d213
Astatine-213-Tian <astatine213.tian@gmail.com>
2021-08-18 19:52:20 +0800

    Change WIP indicator

--- src/components/wipIndicator.tsx
+++ src/components/wipIndicator.tsx
@@ -3,40 +3,31 @@ import {
   IonButtons,
   IonContent,
   IonIcon,
+  IonImg,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { construct } from "ionicons/icons";
+import * as React from "react";
 import { useLocation } from "react-router";
+import ToolbarWithBackButton from "./ToolbarWithBackButton";
 
 export const WIPIndicator = () => {
   const location = useLocation();
   return (
     <IonPage>
-      <IonToolbar>
-        {location.pathname.startsWith("/tabs") ? null : (
-          <IonButtons slot="start">
-            <IonBackButton defaultHref="/" />
-          </IonButtons>
-        )}
-        <IonTitle>糟糕</IonTitle>
-      </IonToolbar>
+      {location.pathname.startsWith("/tabs") ? null : <ToolbarWithBackButton />}
       <IonContent fullscreen>
-        <div
-          style={{
-            display: "flex",
-            height: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ padding: "0 5vw", textAlign: "center" }}>
-            <IonIcon icon={construct} style={{ fontSize: "128px" }} />
-            <div style={{ fontSize: "26px", textAlign: "center" }}>WIP</div>
-            <div style={{ fontSize: "15px", textAlign: "center" }}>
-              这是一个装修页面。我们正在决定他的功能。请过段时间再尝试。
-            </div>
+        <div style={{ padding: "30vh 25vw" }}>
+          <IonImg src={"/assets/icon/no-profile.svg"} />
+          <div
+            style={{
+              marginTop: "30px",
+              textAlign: "center",
+              color: "var(--ion-color-medium)",
+            }}
+          >
+            暂无档案
           </div>
         </div>
       </IonContent>
