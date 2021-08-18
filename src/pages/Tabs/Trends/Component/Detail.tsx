b418b2ca03e071bbde8c76305d9023402796a290
astatine-213-tian <318145208@qq.com>
2021-08-18 19:53:12 +0800

    UI Enhancement

--- src/pages/Tabs/Trends/Component/Detail.tsx
+++ src/pages/Tabs/Trends/Component/Detail.tsx
@@ -59,32 +59,16 @@ export const PostDetail = ({
               style={{ height: "60px", width: "60px", marginRight: "10px" }}
             />
             <IonLabel>
-              <h3>{nickname}</h3>
-              <p>{title}</p>
+              <h2>{nickname}</h2>
+              <p>{title ?? "社员"}</p>
             </IonLabel>
           </IonItem>
-          {/*<div*/}
-          {/*  css={css`*/}
-          {/*    display: flex;*/}
-          {/*    & h3 {*/}
-          {/*      margin-top: 10px;*/}
-          {/*    }*/}
-          {/*    margin-bottom: 8px;*/}
-          {/*  `}*/}
-          {/*>*/}
-          {/*  <UserAvatar src={avatarUrl} style={{ height: "80px", width: "80px"}}/>*/}
-          {/*  <div*/}
-          {/*    css={css`*/}
-          {/*      margin-left: 12px;*/}
-          {/*    `}*/}
-          {/*  >*/}
-          {/*    <h3>{nickname}</h3>*/}
-          {/*    <span>{title}</span>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
+
           <div>{content}</div>
           {!!imageUrls.length && (
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
+            <div
+              style={{ display: "flex", flexWrap: "wrap", marginTop: "8px" }}
+            >
               {imageUrls.map((url) => {
                 return (
                   <Square
