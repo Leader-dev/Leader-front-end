10bcb2f52cdac99e9e2bfbbf6e84850aa7f9c964
astatine-213-tian <318145208@qq.com>
2021-10-11 18:29:41 -0400

    Code enhancement

--- src/services/org/manage/memberInfo/getAccess.ts
+++ src/services/org/manage/memberInfo/getAccess.ts
@@ -8,7 +8,7 @@ interface MemberAccessResult {
   presidentFunctions: boolean;
 }
 
-export const useOrgMemberAccess = (orgId: string) => {
+export const useOrgMemberAccess = ({ orgId }: { orgId: string }) => {
   return useSWR(`/org/manage/member-info/get-access?orgId=${orgId}`, (url) => {
     return axios.post(url).then((res) => res.data.data as MemberAccessResult);
   });
